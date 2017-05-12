5e4df6cb58db972e74140dcaefff0776cc51c9c2
Thay Huynh <thay.huynh@hellopay.com>
2017-05-12 18:11:12 +0700

    Finish Expression

---
+++ week4/.worksheet/src/boolean_implementation.scala
@@ -0,0 +1,9 @@
+object boolean_implementation {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(75); 
+  println("Welcome to the Scala worksheet");$skip(20); 
+  
+  println(false)
+  import week4._;$skip(35); val res$0 = 
+  true1 &&  true1;System.out.println("""res0: week4.Boolean1 = """ + $show(res$0));$skip(18); val res$1 = 
+  true1 && false1;System.out.println("""res1: week4.Boolean1 = """ + $show(res$1))}
+  //println(true1 && true1)
+}
